e5f08faf8197836adfbf2633c26606ee1e319b43
VouttaRene <r.voutta@web.de>
2019-12-13 10:16:02 +0100

    Version 0.04 Database: OnCreate Population added (Just for the Overview)

--- app/src/main/java/de/voutta/einkaufsliste/Database/ShoppingCartDatabase.java
+++ app/src/main/java/de/voutta/einkaufsliste/Database/ShoppingCartDatabase.java
@@ -1,10 +1,13 @@
 package de.voutta.einkaufsliste.Database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = ShoppingCartEntities.class, version = 1, exportSchema = false)
 public abstract class ShoppingCartDatabase extends RoomDatabase {
@@ -18,8 +21,33 @@ public abstract class ShoppingCartDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     ShoppingCartDatabase.class, "ShoppingCart_Databse")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDBAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDBAsyncTask extends AsyncTask<Void, Void, Void>{
+        private ShoppingCartDao shoppingCartDao;
+
+        private PopulateDBAsyncTask(ShoppingCartDatabase db){
+            shoppingCartDao = db.shoppingCartDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            shoppingCartDao.addToShoppingCart(new ShoppingCartEntities("Hähnchen", "400 g"));
+            shoppingCartDao.addToShoppingCart(new ShoppingCartEntities("Sojamilch", "2 Stck"));
+            shoppingCartDao.addToShoppingCart(new ShoppingCartEntities("Joghurt","2"));
+            return null;
+        }
     }
 }
